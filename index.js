fe1781ac1f0f58bbc16b40de004f2fb97ebbeaf7
hadNAU <lhh37@nau.edu>
2026-09-15 22:19:46 -0700

    more files added, made real route for index file

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+// Set EJS as the view engine
+app.set("view engine", "ejs");
+
+// Middleware to parse URL-encoded data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// Route for the home page
 app.get("/", (req, res) => {
-    res.send("blog up");
+    res.render("index");
 });
 
 app.listen(PORT, () => {
